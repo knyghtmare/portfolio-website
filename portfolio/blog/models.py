13d75cba0bd7413192eb2afde7d1096c2d63a297
knyghtmare <tahsinjahinkhalid@gmail.com>
2020-01-31 02:06:56 +0600

    added blogs page

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -6,3 +6,12 @@ class Blog(models.Model):
     post_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to="images/")
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.post_date.strftime("%b %e %Y")
+
+    def __str__(self):
+        return self.title
