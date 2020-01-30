07975d911da234ce58eddb78f3f7aa609f618be4
knyghtmare <tahsinjahinkhalid@gmail.com>
2020-01-31 00:40:43 +0600

    finished the jobs page

--- portfolio/jobs/views.py
+++ portfolio/jobs/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Job
+
 # Create your views here.
 
 def home(request):
-    return render(request, 'jobs/home.html')
+    jobs = Job.objects
+    return render(request, 'jobs/home.html', {'jobs' : jobs} )
